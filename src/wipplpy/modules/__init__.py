28b61e9d2355ddfd1a6cf15cf63d51d62bba60f7
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-09-19 15:39:15 -0500

    Change import * to specific functions

--- src/wipplpy/modules/__init__.py
+++ src/wipplpy/modules/__init__.py
@@ -5,4 +5,8 @@ for loading and modifying data.
 __all__ = ["shot_loader"]
 
 from wipplpy.modules import shot_loader
-from wipplpy.modules.shot_loader import *
+from wipplpy.modules.shot_loader import (
+    get_connector,
+    get_remote_shot_tree,
+    most_recent_shot,
+)
