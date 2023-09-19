27c5ebd3a86b9f7b7c3f1cb52bedceaf6f0a48e0
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-09-19 15:32:47 -0500

    Create init for the modules subdirectory.

---
+++ src/wipplpy/modules/__init__.py
@@ -0,0 +1,8 @@
+"""
+The `~wipplypy.modules` subpackage contains commonly used functions and classes 
+for loading and modifying data.
+"""
+__all__ = []
+
+from wipplpy.modules import shot_loader
+from wipplpy.modules.shot_loader import *
