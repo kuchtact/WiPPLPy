250b3acae7de18952bd17c6d095115d4a99124ef
Cameron Kuchta <cameronkuchta97@yahoo.com>
2023-09-19 17:41:47 -0500

    Add generic_get_data to init

--- src/wipplpy/modules/__init__.py
+++ src/wipplpy/modules/__init__.py
@@ -2,6 +2,9 @@
 The `~wipplypy.modules` subpackage contains commonly used functions and classes
 for loading and modifying data.
 """
-__all__ = ["shot_loader"]
+__all__ = ["shot_loader", "generic_get_data"]
 
-from wipplpy.modules import shot_loader
+from wipplpy.modules import (
+    shot_loader,
+    generic_get_data,
+)
