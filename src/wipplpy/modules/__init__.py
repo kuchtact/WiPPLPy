33b05f7c0339631adcc432fab81430369dd895b6
Cameron Kuchta <ckuchta@wisc.edu>
2023-09-19 15:37:32 -0500

    Update src/wipplpy/modules/__init__.py
    Co-authored-by: Nick Murphy <namurphy@cfa.harvard.edu>

--- src/wipplpy/modules/__init__.py
+++ src/wipplpy/modules/__init__.py
@@ -2,7 +2,7 @@
 The `~wipplypy.modules` subpackage contains commonly used functions and classes 
 for loading and modifying data.
 """
-__all__ = []
+__all__ = ["shot_loader"]
 
 from wipplpy.modules import shot_loader
 from wipplpy.modules.shot_loader import *
