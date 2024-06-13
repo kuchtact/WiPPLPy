1b3aa5ee6d7d57ecdcf6a0e52382c88c4014039b
Nick Murphy <namurphy@cfa.harvard.edu>
2024-06-13 17:05:02 -0400

    Set default backend

--- noxfile.py
+++ noxfile.py
@@ -1,17 +1,23 @@
 import nox
 
 
-PYTHON_VERSIONS=("3.10", "3.11", "3.12")
+supported_python_versions = ("3.10", "3.11", "3.12")
+maxpython = max(supported_python_versions)
+
+# Set the default backend to conda if available, because it is probably
+# needed to install MDSplus installation anyway. The next choice is uv
+# because it has excellent performance when resolving requirements.
+nox.options.default_venv_backend = "conda|uv|virtualenv"
 
 
-@nox.session(python=PYTHON_VERSIONS)
+@nox.session(python=supported_python_versions)
 def tests(session):
     """Run tests with pytest."""
     session.install(".")
     session.run(pytest)
 
 
-@nox.session(python="3.12")
+@nox.session(python=maxpython)
 def build_docs(session):
     """Build documentation with Sphinx."""
     session.install("sphinx")
