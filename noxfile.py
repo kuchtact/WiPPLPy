191b9bab509c5bb1fc1ad4338a26d6133f64aeb6
Nick Murphy <namurphy@cfa.harvard.edu>
2024-06-13 16:49:27 -0400

    Add nox session for pytest

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,16 @@
 import nox
 
 
+PYTHON_VERSIONS=("3.10", "3.11", "3.12")
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def tests(session):
+    """Run tests with pytest."""
+    session.install(".")
+    session.run(pytest)
+
+
 @nox.session(python="3.12")
 def build_docs(session):
     """Build documentation with Sphinx."""
