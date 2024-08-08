ced26b42c260790bafbcfb50dd5542abf1655f59
Cameron Kuchta <cameronkuchta97@yahoo.com>
2024-08-08 14:23:13 -0500

    Add ability to install conda requirements from a yml file.
    A fair bit of this code is taken from https://github.com/wntrblm/nox/issues/260#issuecomment-1565120222

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 import nox
+from yaml import safe_load
 
 supported_python_versions = ("3.9", "3.10", "3.11", "3.12")
 maxpython = max(supported_python_versions)
@@ -8,11 +11,22 @@ maxpython = max(supported_python_versions)
 # because it has excellent performance when resolving requirements.
 nox.options.default_venv_backend = "conda|uv|virtualenv"
 
+# Load in the conda environment file.
+environment = safe_load(Path("mamba_environment.yml").read_text())
+channels = environment.get("channels")
+conda = environment.get("dependencies")
+requirements = conda.pop(-1).get("pip")
 
-@nox.session(python=supported_python_versions)
+
+def install_environment(session):
+    session.conda_install(*conda, channel=channels, silent=False)
+    session.install(*requirements)
+
+
+@nox.session(python=supported_python_versions, venv_backend="mamba")
 def tests(session):
     """Run tests with pytest."""
-    session.install(".")
+    install_environment(session)
     session.run("pytest")
 
 
